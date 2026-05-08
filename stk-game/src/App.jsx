b733b83a860c2342269d0fac2bfcce32ca1e2f4b
Justinbnda <bandiola.justin@yahoo.com>
2026-05-08 17:39:46 +0200

    feat: Mise en place de l'état local et détection des clics sur les cartes

--- stk-game/src/App.jsx
+++ stk-game/src/App.jsx
@@ -1,12 +1,19 @@
 import cardsData from './data/cards.json';
 import './App.css';
 import Card from './Card.jsx';
+import { useState } from 'react';
 
 export default function App() {
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  const handleCardClick = (clickedCard) => {
+    console.log("Carte sélectionnée :", clickedCard.name);
+    // On ajoutera la logique de comparaison ici à la prochaine étape
+  };
   return (
     <main className="stk-board">
       {cardsData.map((card) => (
-        <Card key={card.id} card={card} onClick={(c) => console.log("Clic sur:", c.name)} />
+        <Card key={card.id} card={card} onClick={handleCardClick} />
       ))}
     </main>
   );
